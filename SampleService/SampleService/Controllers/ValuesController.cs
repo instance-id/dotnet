d0d1c55e5df617fe89d602bc143015b849886855
instance-id <dan@instance.id>
2019-06-30 19:07:05 -0500

    Initial commit of dotNet build pipeline demo

---
+++ SampleService/SampleService/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleService.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Operation:", "Successful" };
+        }
+    }
+}
